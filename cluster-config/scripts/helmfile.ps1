151802eb7275fe8ee6edf3acc3223a397ee8d8e2
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-03-23 10:48:52 +0100

    Add azuredisk-csi-driver

---
+++ cluster-config/scripts/helmfile.ps1
@@ -0,0 +1,6 @@
+docker run --rm --net=host `
+    -v "$env:USERPROFILE/.kube:/helm/.kube" `
+    -v "$env:USERPROFILE/.config/helm:/helm/.config/helm" `
+    -v "$(Get-Location):/wd" --workdir /wd `
+    ghcr.io/helmfile/helmfile:v0.163.0 `
+    helmfile $args
