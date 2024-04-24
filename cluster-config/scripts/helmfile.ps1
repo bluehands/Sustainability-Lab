bfb2e5509febf623d622e147653d0f7c5a049d39
Waldemar Tomme <waldemar.tomme@bluehands.de>
2024-04-24 18:36:18 +0200

    Scale load-app by requests

--- cluster-config/scripts/helmfile.ps1
+++ cluster-config/scripts/helmfile.ps1
@@ -1,4 +1,9 @@
 #!/usr/bin/env pwsh
+
+if ($IsLinux) {
+    $env:USERPROFILE = $env:HOME
+}
+
 docker run --rm --net=host `
     -v "$env:USERPROFILE/.kube:/helm/.kube" `
     -v "$env:USERPROFILE/.config/helm:/helm/.config/helm" `
